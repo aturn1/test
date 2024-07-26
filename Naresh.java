f9c83ad9103c8b49a3eba543f2d3f5fa5f4229f4
aturn1 <naresh.batch001@gmail.com>
2024-07-26 18:25:18 +0530

    Create Naresh.java

---
+++ Naresh.java
@@ -0,0 +1,11 @@
+public class Naresh
+  {
+    public static void main(String[] args)
+    {
+      System.out.println("Naresh IT DevOps Course Curriculum");
+      System.out.println("Linux and Shell");
+      System.out.println("Git and Github");
+      System.out.println("Jenkins");
+    }
+  }
+      
